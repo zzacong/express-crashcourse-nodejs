dd91c800f720f70ad3d1648fa6a801a529e90b4f
zzacong <ongzz100@gmail.com>
2020-10-19 12:44:29 +1100

    rename to server.js

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use('/api/members', require('./routes/api/members'))
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is listening on port ${PORT}`)
 })
